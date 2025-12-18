d3e8153d1d8c259b5f903712b8cdf68a244d19bb
Snider <snider@lt.hn>
2025-12-18 14:28:20 +0000

    add build configuration script for BTCPay setup

---
+++ 0-build-config.sh
@@ -0,0 +1,10 @@
+export BTCPAY_HOST="pay.host.uk.com"
+export NBITCOIN_NETWORK="mainnet"
+export BTCPAYGEN_CRYPTO1="btc"
+export BTCPAYGEN_ADDITIONAL_FRAGMENTS="opt-save-storage-s"
+export BTCPAYGEN_REVERSEPROXY="none"
+export BTCPAYGEN_LIGHTNING="clightning"
+export REVERSEPROXY_HTTP_PORT=80
+export BTCPAY_ENABLE_SSH="true"
+export CHATWOOT_HOST="chat.host.uk.com"
+. ./btcpay-setup.sh --i
